a636d3518eeab3276e3ae4d5f12af785d98ce303
andre <andre.b.otte1@gmail.com>
2019-04-22 14:22:44 -0400

    lab-2

---
+++ lab-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
